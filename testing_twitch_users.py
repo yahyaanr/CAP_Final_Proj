af4f214f95e25b36b81f394e69805f0483af497e
mocha <yahyaanwar73@gmail.com>
2023-06-01 08:09:45 +0700

    Added Time_files Feature and bash

--- testing_twitch_users.py
+++ testing_twitch_users.py
@@ -6,40 +6,55 @@ import aiohttp
 import time
 from tqdm.asyncio import tqdm_asyncio as tqdm
 import multiprocessing
+from datetime import datetime
+import subprocess
+from functools import wraps
 
 load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
-tsv_file_path = '/home/mocha/cap/Final_Proj/testing_user.tsv'
-tsv_file_path1 = '/home/mocha/cap/Final_Proj/testing_twitch_stream_.tsv'
+
+tsv_file_user_uni = '/home/mocha/cap/Final_Proj/tsv/user_centralized.tsv'
+tsv_file_stream_uni = '/home/mocha/cap/Final_Proj/tsv/stream_centralized.tsv'
+timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+tsv_file_user_time = f"/home/mocha/cap/Final_Proj/tsv/user_{timestamp}.tsv"
+hdfs_path = f'/user/mocha/final/user/user_{timestamp}.tsv'
+
 column_indices_1 = [0]
 column_indices_2 = [2]
 
-def save_user_to_csv(user_data, tsv_file_path):
-    with open(tsv_file_path, 'a', newline='') as file:
+def save_user_to_csv(user_data, tsv_file_user_uni, tsv_file_user_time):
+    row = [
+        user_data['id'],
+        user_data['login'],
+        user_data['display_name'],
+        user_data['type'],
+        user_data['broadcaster_type'],
+        user_data['description'],
+        user_data['profile_image_url'],
+        user_data['offline_image_url'],
+        user_data['view_count'],
+        user_data['created_at']
+    ]
+    print("Saving user:", user_data['login'])  # Debugging statement
+    with open(tsv_file_user_uni, 'a', newline='') as file:
         writer = csv.writer(file, delimiter='\t')
-        writer.writerow([
-            user_data['id'],
-            user_data['login'],
-            user_data['display_name'],
-            user_data['type'],
-            user_data['broadcaster_type'],
-            user_data['description'],
-            user_data['profile_image_url'],
-            user_data['offline_image_url'],
-            user_data['view_count'],
-            user_data['created_at']
-        ])
+        writer.writerow(row)
 
-def read_columns_from_tsv(tsv_file_path, column_indices):
-    if not os.path.exists(tsv_file_path):
+    # Append the data to the unified origin data file
+    with open(tsv_file_user_time, 'a', newline='') as unified_file:
+        writer = csv.writer(unified_file, delimiter='\t')
+        writer.writerow(row)
+
+def read_columns_from_tsv(tsv_file_user_uni, column_indices):
+    if not os.path.exists(tsv_file_user_uni):
         # Create an empty file
-        with open(tsv_file_path, 'w'):
+        with open(tsv_file_user_uni, 'w'):
             pass
 
     column_values = []
-    with open(tsv_file_path, 'r') as file:
+    with open(tsv_file_user_uni, 'r') as file:
         reader = csv.reader(file, delimiter='\t')
         try:
             header = next(reader)  # Read the header row
@@ -50,15 +65,16 @@ def read_columns_from_tsv(tsv_file_path, column_indices):
             column_values.append(values)
     return column_values
 
-def compare_unique_ids(tsv_file_path_1, tsv_file_path_2, column_indices_1, column_indices_2):
-    unique_ids_1 = set(tuple(values) for values in read_columns_from_tsv(tsv_file_path_1, column_indices_1))
-    unique_ids_2 = set(tuple(values) for values in read_columns_from_tsv(tsv_file_path_2, column_indices_2))
+def compare_unique_ids(tsv_file_user_uni, tsv_file_stream_uni, column_indices_1, column_indices_2):
+    unique_ids_1 = set(tuple(values) for values in read_columns_from_tsv(tsv_file_user_uni, column_indices_1))
+    unique_ids_2 = set(tuple(values) for values in read_columns_from_tsv(tsv_file_stream_uni, column_indices_2))
     unique_ids_correlated = unique_ids_1.intersection(unique_ids_2)  # Find the IDs that are present in both sets
-    unique_ids_not_correlated = list(unique_ids_2 - unique_ids_correlated)  # Find the IDs in unique_ids_2 that are not in unique_ids_1
+    unique_ids_not_correlated = unique_ids_2 - unique_ids_correlated  # Find the IDs in unique_ids_2 that are not in unique_ids_1
     count_user = len(unique_ids_1)
     count_before = len(unique_ids_2)
     count = len(unique_ids_not_correlated)
     return count_user, count_before, count, unique_ids_not_correlated
+
 
 class TokenBucket:
     def __init__(self, rate_limit, bucket_size):
@@ -89,9 +105,9 @@ async def get_twitch_user(session, access_token, user_id, headers):
         else:
             return None
 
-async def get_twitch_users(access_token, stream_ids, tsv_file_path, save_interval=1000):
+async def get_twitch_users(access_token, stream_ids, tsv_file_user_uni, tsv_file_user_time, save_interval=1000):
     
-    count_user, count_before, count, unique_ids_not_correlated = compare_unique_ids(tsv_file_path, tsv_file_path1, column_indices_1, column_indices_2)
+    count_user, count_before, count, unique_ids_not_correlated = compare_unique_ids(tsv_file_user_uni, tsv_file_stream_uni, column_indices_1, column_indices_2)
     
     print("Total Unique ID on Stream Table: ", count_before)
     print("Total Unique ID on User Table: ", count_user)
@@ -106,8 +122,8 @@ async def get_twitch_users(access_token, stream_ids, tsv_file_path, save_interva
     start_time = time.time()
     request_counter = 0
 
-    if not os.path.isfile(tsv_file_path):
-        with open(tsv_file_path, 'w', newline='') as file:
+    if not os.path.isfile(tsv_file_user_uni):
+        with open(tsv_file_user_uni, 'w', newline='') as file:
             header = [
                 'id',
                 'login',
@@ -136,7 +152,7 @@ async def get_twitch_users(access_token, stream_ids, tsv_file_path, save_interva
             data = await response.json()
             if 'data' in data and len(data['data']) > 0:
                 user_info = data['data'][0]
-                save_user_to_csv(user_info, tsv_file_path)
+                save_user_to_csv(user_info, tsv_file_user_uni, tsv_file_user_time)
                 nonlocal save_count
                 save_count += 1
 
@@ -186,9 +202,30 @@ async def get_twitch_users(access_token, stream_ids, tsv_file_path, save_interva
     print(f'Saved data for {save_count} users in total.')
     pbar.close()  # Close the progress bar
 
+def write_to_hdfs(file_path, hdfs_path):
+    bash_command = f'hadoop fs -put {file_path} {hdfs_path}'
+    subprocess.run(bash_command, shell=True)
+
+with open(tsv_file_user_time, 'w', newline='') as unified_origin_file:
+    header = [
+        'id',
+        'login',
+        'display_name',
+        'type',
+        'broadcaster_type',
+        'description',
+        'profile_image_url',
+        'offline_image_url',
+        'view_count',
+        'created_at'
+    ]
+    writer = csv.writer(unified_origin_file, delimiter='\t')
+    writer.writerow(header)
 
 # Read stream IDs from the TSV file
-stream_ids = [item[0] for item in compare_unique_ids(tsv_file_path, tsv_file_path1, column_indices_1, column_indices_2)[-1]]
+stream_ids = [item[0] for item in compare_unique_ids(tsv_file_user_uni, tsv_file_stream_uni, column_indices_1, column_indices_2)[-1]]
 
 save_interval = 1000  # Optional: Specify the save interval
-asyncio.run(get_twitch_users(access_token, stream_ids, tsv_file_path, save_interval))
+asyncio.run(get_twitch_users(access_token, stream_ids, tsv_file_user_uni, tsv_file_user_time, save_interval))
+
+write_to_hdfs(tsv_file_user_time, hdfs_path)
